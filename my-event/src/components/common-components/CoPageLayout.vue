d8ed19a754659620b675072f5986acd96a3f7e05
emresimsek <emresimseka@gmail.com>
2021-09-26 14:21:46 +0300

    feature(co-page-layout):

---
+++ my-event/src/components/common-components/CoPageLayout.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="d-flex flex-column co-page-layout" :class="computedClass">
+    <div class="d-flex justify-content-center pages-areas">
+      <div :class="leftClass" class="left">
+        <slot name="left-side"></slot>
+      </div>
+      <div :class="mainClass" class="main">
+        <slot> </slot>
+      </div>
+      <div :class="rightClass" class="right">
+        <slot name="right-side"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import BaseComponent from "./BaseComponent.vue";
+@Options({
+  components: { BaseComponent },
+  props: {
+    position: String,
+    rightClass: String,
+    leftClass: String,
+    mainClass: String,
+  },
+})
+export default class CoPageLayout extends BaseComponent {
+  position!: string;
+  rightClass!: string;
+  leftClass!: string;
+  mainClass!: string;
+
+  get computedClass() {
+    return [this.position ? `position-${this.position}` : ""];
+  }
+}
+</script>
+<style>
+</style>
+
